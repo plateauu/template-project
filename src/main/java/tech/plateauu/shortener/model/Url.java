29b763ed0fd338ea3ce1d0a011896dba440d5a01
Marcin Michalak <plateauu@o2.pl>
2017-11-26 12:53:11 +0100

    Gradle configured

--- src/main/java/tech/plateauu/shortener/model/Url.java
+++ src/main/java/tech/plateauu/shortener/model/Url.java
@@ -4,19 +4,29 @@ package tech.plateauu.shortener.model;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import tech.plateauu.shortener.utils.LocalDateTimeDeserializer;
 import tech.plateauu.shortener.utils.LocalDateTimeSerializer;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 /**
  * Model entity
  */
+@Entity
 @Data
+@EqualsAndHashCode
 public class Url {
 
+    @GeneratedValue
+    @Id
     private final long id;
+
     private final String shortUrl;
+
     private final String longUrl;
 
     @JsonSerialize(using = LocalDateTimeSerializer.class)
--- src/test/groovy/tech/plateauu/shortener/SpringContextTest.groovy
+++ src/test/groovy/tech/plateauu/shortener/SpringContextTest.groovy
@@ -3,11 +3,11 @@ package tech.plateauu.shortener
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Profile
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [ShortenerApplication.class])
-@Profile("test")
+@ContextConfiguration
 class SpringContextTest extends Specification {
 
     @Autowired

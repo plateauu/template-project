a3116f4a308c220f453e91c3482256ab7b525caa
Marcin Michalak <plateauu@o2.pl>
2017-11-27 07:20:56 +0100

    Starting to configuring data source. Added new bean, and configuration.

--- src/main/java/tech/plateauu/shortener/ShortenerApplication.java
+++ src/main/java/tech/plateauu/shortener/ShortenerApplication.java
@@ -1,13 +1,27 @@
 package tech.plateauu.shortener;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @SpringBootApplication
 public class ShortenerApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication
                 .run(ShortenerApplication.class, args);
+
+    }
+
+    class ScheduledCommandLineRunner implements CommandLineRunner {
+
+        @Scheduled()
+        @Override
+        public void run(String... args) throws Exception {
+            System.out.println("Hello world");
+        }
     }
 }
